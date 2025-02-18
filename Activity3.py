b9234a606a0834f1d76afd39598c331e9cd5f499
dhavaniamin1331 <a.nirali266@gmail.com>
2025-02-18 18:18:40 +0200

    Activity3

---
+++ Activity3.py
@@ -0,0 +1,13 @@
+def leaders(a, a_size):
+    # Right most is always a leader 
+    currentmax = a[a_size-1]
+    print(currentmax)
+
+    # Check from 2nd last element if it is greater than currentmax is yes that number is a leader and our new currentmax too.
+    for i in range( a_size-2, -1, -1):
+        if currentmax < a[i]:
+            print(a[i])
+            currentmax = a[i]
+
+a = [16, 17, 4, 3, 5, 245]
+leaders(a, len(a))
